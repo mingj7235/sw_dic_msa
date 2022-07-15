76e8a7c2176b604055a83bdcf8f9e2d79e04378c
mingj7235 <joshuara7235@gmail.com>
2022-07-15 10:24:47 +0900

    Sw user-service git init

---
+++ sw-user-service/src/main/java/com/example/swuserservice/SwUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.swuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwUserServiceApplication.class, args);
+    }
+
+}
---
+++ sw-user-service/src/test/java/com/example/swuserservice/SwUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
