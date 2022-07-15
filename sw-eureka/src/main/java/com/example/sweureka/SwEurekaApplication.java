3fd7d4298f2c20f755d6041855c06fb57e0f3d2a
mingj7235 <joshuara7235@gmail.com>
2022-07-15 11:03:22 +0900

    SW Eureka server git init

---
+++ sw-eureka/src/main/java/com/example/sweureka/SwEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sweureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwEurekaApplication.class, args);
+	}
+
+}
---
+++ sw-eureka/src/test/java/com/example/sweureka/SwEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
