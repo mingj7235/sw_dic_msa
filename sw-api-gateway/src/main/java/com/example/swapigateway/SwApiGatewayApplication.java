a2243c8cf4066c2e297181b09372c928affd5be8
mingj7235 <joshuara7235@gmail.com>
2022-07-15 12:08:39 +0900

    Api gateway git init and filter, yml configuration

---
+++ sw-api-gateway/src/main/java/com/example/swapigateway/SwApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.swapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwApiGatewayApplication.class, args);
+    }
+
+}
---
+++ sw-api-gateway/src/main/java/com/example/swapigateway/filter/LoggingFilter.java
@@ -0,0 +1,32 @@
+package com.example.swapigateway.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+
+    public LoggingFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(final Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+        }
+    }
+
+    @Data
+    public static class Config {
+
+    }
+}
