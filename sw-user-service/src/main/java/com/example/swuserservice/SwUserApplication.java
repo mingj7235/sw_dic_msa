a9a098c05e01e9b11c7f712312ada85c7e49e2ef
mingj7235 <joshuara7235@gmail.com>
2022-07-15 11:15:35 +0900

    1. User service -> eureka client configuration on application class
    2. Eureka service -> yml configuration, eureka server configuration on application class

--- sw-user-service/src/main/java/com/example/swuserservice/SwUserApplication.java
+++ sw-user-service/src/main/java/com/example/swuserservice/SwUserApplication.java
@@ -2,8 +2,11 @@ package com.example.swuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SwUserApplication {
 
     public static void main(String[] args) {
